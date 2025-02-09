f4b73473fe0e2c456ee78cd520723aab6ed40041
RyanMichael Hirst <ryanmichaelhirst@gmail.com>
2025-02-09 16:30:57 -0500

    Add landing page

--- app/routes/_app/_index/_home.tsx
+++ app/routes/_app/_index/_home.tsx
@@ -1,25 +1,37 @@
-import { ButtonLink } from "@/components/ButtonLink"
+import landingBg from "@/assets/landing_bg.png"
+import { Button } from "@/components/ui/button"
 import { type MetaFunction } from "@remix-run/node"
-import { route } from "routes-gen"
-
+import { useNavigate } from "@remix-run/react"
 export const meta: MetaFunction = () => [{ title: "OPTCG Api | Home" }]
 
 export default function Page() {
+  const navigate = useNavigate()
+
   return (
-    <main>
-      <div className="mx-auto max-w-7xl space-y-8 px-4 pb-16 pt-20 text-center sm:px-6 lg:px-8 lg:pt-32">
-        <div className="flex flex-col items-center justify-center">
-          <div className="mb-6 text-center text-6xl font-semibold">Welcome to OPTCG Api</div>
-        </div>
-        <div className="flex items-center justify-center space-x-3">
-          <ButtonLink
-            to={route("/")}
-            size="lg"
-            variant="default"
-            className="rounded-full font-semibold"
+    <main className="relative h-screen w-full">
+      <div
+        className="absolute inset-0 bg-cover bg-center"
+        style={{ backgroundImage: `url(${landingBg})` }}
+      >
+        <div className="absolute inset-0 bg-gray-900 bg-opacity-60 dark:bg-gray-600 dark:bg-opacity-20"></div>
+      </div>
+
+      <div className="relative z-10 flex h-full flex-col items-center justify-center">
+        <p className="mb-10 text-4xl font-bold text-white">OPTCG Api</p>
+        <div className="flex max-w-xl flex-col items-center">
+          <p className="text-xl text-white">
+            Access all One Piece trading cards from the web or api. Perfect for deck building and
+            personal projects.
+          </p>
+          <Button
+            onClick={() => {
+              window.open("/docs", "_self")
+            }}
+            className="mt-4"
+            variant="outline"
           >
-            Get Started
-          </ButtonLink>
+            Get started
+          </Button>
         </div>
       </div>
     </main>
