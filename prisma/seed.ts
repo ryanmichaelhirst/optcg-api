70cad88d0f979d385def835184ae13e2834845d5
RyanMichael Hirst <ryanmichaelhirst@gmail.com>
2025-02-08 19:49:31 -0500

    Add scripts/db-seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,11 +1,8 @@
 import { db } from "@/lib/db.server"
 import { fakerEN_US as faker } from "@faker-js/faker"
+import { seedCards } from "scripts/db-seed"
 
-import fs from "fs"
-import path from "path"
-import { SeedCardData } from "scripts/scrape-optcg"
-
-async function seed() {
+export async function seed() {
   const iterations = Array.from(Array(10).keys())
   for (const iter of iterations) {
     await db.user.create({
@@ -18,38 +15,7 @@ async function seed() {
   }
 
   try {
-    const files = fs.readdirSync(path.join(process.cwd(), "tmp"))
-    if (files.length === 0) {
-      throw new Error("No card data files found in /tmp. Did you run the scraper?")
-    }
-
-    for (const file of files) {
-      const filePath = path.join(process.cwd(), "tmp", file)
-
-      const content = fs.readFileSync(filePath, "utf-8")
-      const jsonData: SeedCardData[] = JSON.parse(content)
-      for (const card of jsonData) {
-        await db.card.create({
-          data: {
-            name: card.cardName,
-            cost: Number(card.cost.replace(/cost/gi, "")),
-            attribute: card.attribute,
-            power: Number(card.power),
-            counter: Number(card.counter),
-            color: card.color,
-            class: card.class,
-            ...(card.effect.trim().length > 1 && {
-              effect: card.effect,
-            }),
-            set: card.cardSet,
-            code: card.infoCol[0],
-            rarity: card.infoCol[1],
-            type: card.infoCol[2],
-            image: card.image,
-          },
-        })
-      }
-    }
+    await seedCards()
   } catch (err) {
     console.error(err)
   }
---
+++ scripts/db-seed.ts
@@ -0,0 +1,48 @@
+import { db } from "@/lib/db.server"
+import { Effect } from "effect"
+// import { fakerEN_US as faker } from "@faker-js/faker"
+
+import fs from "fs"
+import path from "path"
+import { SeedCardData } from "scripts/scrape-optcg"
+
+const program = Effect.gen(function* () {
+  yield* Effect.promise(() => seedCards())
+})
+
+export async function seedCards() {
+  const files = fs.readdirSync(path.join(process.cwd(), "tmp"))
+  if (files.length === 0) {
+    throw new Error("No card data files found in /tmp. Did you run the scraper?")
+  }
+
+  for (const file of files) {
+    const filePath = path.join(process.cwd(), "tmp", file)
+
+    const content = fs.readFileSync(filePath, "utf-8")
+    const jsonData: SeedCardData[] = JSON.parse(content)
+    for (const card of jsonData) {
+      await db.card.create({
+        data: {
+          name: card.cardName,
+          cost: Number(card.cost.replace(/cost/gi, "")),
+          attribute: card.attribute,
+          power: Number(card.power),
+          counter: Number(card.counter),
+          color: card.color,
+          class: card.class,
+          ...(card.effect.trim().length > 1 && {
+            effect: card.effect,
+          }),
+          set: card.cardSet,
+          code: card.infoCol[0],
+          rarity: card.infoCol[1],
+          type: card.infoCol[2],
+          image: card.image,
+        },
+      })
+    }
+  }
+}
+
+Effect.runPromiseExit(program).then(console.log)
