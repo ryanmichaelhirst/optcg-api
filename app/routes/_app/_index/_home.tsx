1d799eef2638b6b2e9438af1bcc62f52b1481086
RyanMichael Hirst <ryanmichaelhirst@gmail.com>
2025-02-10 16:13:11 -0500

    Add github link

--- app/routes/_app/_index/_home.tsx
+++ app/routes/_app/_index/_home.tsx
@@ -13,25 +13,31 @@ export default function Page() {
         className="absolute inset-0 bg-cover bg-center"
         style={{ backgroundImage: `url(${landingBg})` }}
       >
-        <div className="absolute inset-0 bg-gray-900 bg-opacity-60 dark:bg-gray-600 dark:bg-opacity-20"></div>
+        <div className="absolute inset-0 bg-gray-900 bg-opacity-60 dark:bg-black dark:bg-opacity-70"></div>
       </div>
 
       <div className="relative z-10 flex h-full flex-col items-center justify-center">
         <p className="mb-10 text-4xl font-bold text-white">OPTCG Api</p>
         <div className="flex max-w-xl flex-col items-center">
-          <p className="text-xl text-white">
-            Access all One Piece trading cards from the web or api. Perfect for deck building and
-            personal projects.
+          <p className="mb-4 text-xl text-white">
+            Access all One Piece trading cards from the web or api. Open source. Perfect for deck
+            building and personal projects.
           </p>
-          <Button
-            onClick={() => {
-              window.open("/docs", "_self")
-            }}
-            className="mt-4"
-            variant="outline"
-          >
-            Get started
-          </Button>
+          <div className="flex items-center justify-center gap-x-4">
+            <Button
+              onClick={() => {
+                window.open("/docs", "_self")
+              }}
+              variant="outline"
+            >
+              Get started
+            </Button>
+            <Button
+              onClick={() => window.open("https://github.com/ryanmichaelhirst/optcg-api", "_blank")}
+            >
+              Github
+            </Button>
+          </div>
         </div>
       </div>
     </main>
